c5dd8f29b6e90a88c0e8a99ae23e8ac2e9d5df4a
MinhTruongDev <104303855+MinhTruongDev@users.noreply.github.com>
2024-04-30 16:33:15 +0700

    feat: add button with event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,12 +8,24 @@ class MyComponent extends React.Component {
         age: 25,
     };
 
+    handleClick(event) {
+        console.log('My Name is ', this.state.name);
+        // console.log(event);
+
+    };
+
+
+    handleMouseOver(event) {
+        console.log(event.pageX, event.pageY);
+
+    };
+
     // JSX
     render() {
         return (
             <div>
                 My name is {this.state.name} but people call me {this.state.nickname}
-
+                <button onClick={this.handleClick} onMouseOver={this.handleMouseOver}>Click me</button>
             </div>
         );
     }
