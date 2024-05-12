ff8fe2948908a4393c672dc252d30f10dbcc2ba2
MinhTruongDev <104303855+MinhTruongDev@users.noreply.github.com>
2024-05-12 19:43:57 +0700

    feat: Add route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.scss';
 import Header from './components/Header/Header';
-
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className='app-container'>
       <Header />
-
+      <div>
+        Test Link
+        <div>
+          <button>
+            <Link to={`/users`}>Go To User Page</Link>
+          </button>
+          <button>
+            <Link to={`/admins`}>Go To Admin Page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import { Link } from "react-router-dom";
 const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">MinhTruongDev</Navbar.Brand>
+                <Navbar.Brand href="/">MinhTruongDev</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#user">User</Nav.Link>
-                        <Nav.Link href="#admin">Admin</Nav.Link>
-
+                        <Link to='/' className='nav-link'>Home</Link>
+                        <Link to='/users' className='nav-link'>User</Link>
+                        <Link to='/admins' className='nav-link'>Admin</Link>
+                        {/* <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/users">User</Nav.Link>
+                        <Nav.Link href="/admins">Admin</Nav.Link> */}
                     </Nav>
                     <Nav>
                         <NavDropdown title="Settings" id="basic-nav-dropdown">
