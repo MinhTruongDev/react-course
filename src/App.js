532f8fc81deba4c9c853ca84fc0f14e6967df936
MinhTruongDev <104303855+MinhTruongDev@users.noreply.github.com>
2024-05-12 19:01:44 +0700

    Add bootstrap and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
-import React from 'react';
 
 
 const App = () => {
   return (
-    <>
-      Hello &amp; Trường
-      <MyComponent />
-    </>
+    <div className='app-container'>
+      Hello world
+
+    </div>
   );
 }
 
