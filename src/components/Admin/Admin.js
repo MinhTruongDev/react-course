e9f22d00f529992f8ff292f3e7101221d63e5d1d
MinhTruongDev <104303855+MinhTruongDev@users.noreply.github.com>
2024-05-15 22:22:23 +0700

    feat: add sidebar for admin

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,10 +1,19 @@
 import Header from "../Header/Header";
-
+import SideBar from "./SideBar";
+import './Admin.scss';
+import { FaBars } from 'react-icons/fa';
+import { useState } from "react";
 const Admin = (props) => {
+    const [collapsed, setCollapsed] = useState(false);
     return (
         <>
-            <div>
-                Admin Component
+            <div className="admin-container">
+                <div className="admin-sidebar">
+                    <SideBar collapsed={collapsed} />
+                </div>
+                <div className="admin-content">
+                    <FaBars onClick={() => setCollapsed(!collapsed)} />Admin content
+                </div>
             </div>
         </>
     );
